030892448ae1022f151535e5acbd758a75ef5429
WamWooWam <wamwoowam@gmail.com>
2018-03-21 20:13:33 +0000

    fuck oops

--- WamBotRewrite/Api/CommandContext.cs
+++ WamBotRewrite/Api/CommandContext.cs
@@ -114,7 +114,7 @@ namespace WamBotRewrite.Api
             }
             else
             {
-                Channel.SendMessageAsync(content, false, emb);
+                await Channel.SendMessageAsync(content, false, emb);
             }
         }
 
